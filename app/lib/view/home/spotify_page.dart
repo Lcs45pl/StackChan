9dcb0f62a3a2357c7ea1a7cc7bf87da39cc83ca3
Lcs45pl <lucas@cyril-checroun.storga.com>
2026-07-04 13:34:50 +0200

    Create SpotifyPage with Cupertino design
    Added a new SpotifyPage widget with a basic UI.

--- app/lib/view/home/spotify_page.dart
+++ app/lib/view/home/spotify_page.dart
@@ -1,1 +1,40 @@
+ Import 'package:flutter/cupertino.dart';
 
+
+class SpotifyPage extends StatefulWidget {
+
+const SpotifyPage({Key? key}) : super(key: key);
+
+
+@override
+
+State<SpotifyPage> createState() => _SpotifyPageState();
+
+}
+
+
+class _SpotifyPageState extends State<SpotifyPage> {
+
+@override
+
+Widget build(BuildContext context) {
+
+return const CupertinoPageScaffold(
+
+navigationBar: CupertinoNavigationBar(
+
+middle: Text('Spotify'),
+
+),
+
+child: Center(
+
+child: Text('Fonctionnalité Spotify en cours de développement'),
+
+),
+
+);
+
+}
+
+} 
